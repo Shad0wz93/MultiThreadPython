2a4abf1e9af4a05618d15a10ab48afbfb3bc11f4
Adam <adamboissy2004@gmail.com>
2026-03-26 11:00:04 +0100

    squelette bank

---
+++ compte.py
@@ -0,0 +1,22 @@
+import threading
+
+class Compte:
+    def __init__(self, numero, solde_initial=0):
+        self.numero = numero
+        self._solde = solde_initial
+        self._lock = threading.Lock()
+
+    def deposer(self, montant):
+        with self._lock:
+            self._solde += montant  # TODO compléter
+
+    def retirer(self, montant) -> bool:
+        with self._lock:
+            if self._solde >= montant:
+                self._solde -= montant
+                return True
+            return False  # solde insuffisant
+
+    def get_solde(self) -> float:
+        with self._lock:
+            return self._solde
