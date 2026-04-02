249df6c6625f4d24f76fe0cacef680232b814495
Victoria Pea <victoriapea@outlook.fr>
2026-04-02 13:50:11 +0200

    changement nom attribut

--- dab.py
+++ dab.py
@@ -7,13 +7,13 @@ class DAB:
   # Distributeur automatique. Sémaphore(N) limite à N retraits parallèles.
   def __init__(self, nb_dab: int = 2):
     self.nb_dab = nb_dab
-    self.__semaphore = threading.Semaphore(nb_dab)  # Compteur interne = nb_dab
+    self.dab = threading.Semaphore(nb_dab)  # Compteur interne = nb_dab
 
   def retirer(self, compte: Compte, montant: float, nom_client: str) -> bool:
     # Retrait au DAB. Bloque si tous occupés, réveil auto au release().
     print(f"[DAB] {nom_client} attend un DAB disponible...")
 
-    with self.__semaphore:  # acquire() + release() automatiques
+    with self.dab:  # acquire() + release() automatiques
       # Si compteur > 0 : entre, compteur--
       # Si compteur = 0 : BLOQUE jusqu'au prochain release()
       print(f"[DAB] {nom_client} utilise un DAB")
